7789761bef1baf2387744f4bc3d0eb65b852d7ac
GalZisin <46171488+GalZisin@users.noreply.github.com>
2019-09-03 01:18:14 +0300

    Add files via upload

---
+++ AirlineManagement/Facade/Interfaces/IAnonymousUserFacade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineManagement
+{
+    public interface IAnonymousUserFacade
+    {
+        IList<Flight> GetAllFlights();
+        IList<AirlineCompany> GetAllAirlineCompanies();
+        Dictionary<Flight, int> GetAllFlightsByVacancy();
+        Flight GetFlightById(long flightId);
+        IList<Flight> GetFlightsByOriginCountry(long countryCode);
+        IList<Flight> GetFlightsByDestinationCountry(long countryCode);
+        IList<Flight> GetFlightsByDepatrureDate(DateTime departureDate);
+        IList<Flight> GetFlightsByLandingDate(DateTime landingDate);
+    }
+}
